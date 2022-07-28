4da4336c08efa3dcfb283ca8f1764fc5bc9ac9d1
matiaspierpaoli <90465662+matiaspierpaoli@users.noreply.github.com>
2022-07-28 11:43:04 -0300

    Corregidos los mensajes de victoria y derrota

--- Final_P1_Pierpaoli_Matias/Ejercicio 1/GameManager.cpp
+++ Final_P1_Pierpaoli_Matias/Ejercicio 1/GameManager.cpp
@@ -10,6 +10,7 @@ GameManager::GameManager()
 	for (int i = 0; i < asteroidsAmount; i++)
 	{		
 		asteroids[i] = new Asteroid(new Position(rand() % 71 + 3, rand() % 10 + 6), new Size(1,1));
+		playerStats.addActiveAsteroids();
 	}
 
 	hideCursor();
@@ -43,104 +44,104 @@ void GameManager::gameLoop()
 
 void GameManager::drawScreen()
 {
-	playerStats.drawLives(((Ship*)ship)->getCurrentLives());
-	playerStats.drawScore(((Ship*)ship)->getCurrentScore());
-	playerStats.drawActiveAsteroids();
-
-	hud.drawFrameHUD();
-
-	if (((Ship*)ship)->getCurrentLives() <= 0)
-		((Ship*)ship)->explode();
-	else	
-		ship->draw(ship->getPosition().getX(), ship->getPosition().getY());
-	
-	for (auto it = bullets.begin(); it != bullets.end(); it ++)
-	{		
-		(*it)->draw((*it)->getPosition().getX(), (*it)->getPosition().getY());
-	}
-
-	for (int i = 0; i < asteroidsAmount; i++)
+	if (!exit)
 	{
-		asteroids[i]->draw(asteroids[i]->getPosition().getX(), asteroids[i]->getPosition().getY());
-	}	
+		playerStats.drawLives(((Ship*)ship)->getCurrentLives());
+		playerStats.drawScore(((Ship*)ship)->getCurrentScore());
+		playerStats.drawActiveAsteroids();
+
+		hud.drawFrameHUD();
+
+		if (((Ship*)ship)->getCurrentLives() <= 0)
+			((Ship*)ship)->explode();
+		else
+			ship->draw(ship->getPosition().getX(), ship->getPosition().getY());
+
+		for (auto it = bullets.begin(); it != bullets.end(); it++)
+		{
+			(*it)->draw((*it)->getPosition().getX(), (*it)->getPosition().getY());
+		}
+
+		for (int i = 0; i < asteroidsAmount; i++)
+		{
+			asteroids[i]->draw(asteroids[i]->getPosition().getX(), asteroids[i]->getPosition().getY());
+		}
+	}
 }
 
 void GameManager::updateScreen()
 {
-	if(_kbhit())
+	if (!exit)
 	{
-		char tecla = _getch();
-
-		if (tecla == 'a' && ship->getPosition().getX() > 3) ship->travelLeft();
-		if (tecla == 'd' && ship->getPosition().getX() + ship->getSize().getWidth() < getScreenWidth() - 1) ship->travelRight();
-		if (tecla == 'e')
-		{			
-			bullets.push_back(new Bullet(new Position(ship->getPosition().getX() + ship->getSize().getWidth() / 2 - 1, ship->getPosition().getY() - 1), new Size(1, 1)));
-		}		
-	}
-	
-	for (auto it = bullets.begin(); it != bullets.end();)
-	{
-		(*it)->travelUp();
-		if ((*it)->outOfBounds())
+		if (_kbhit())
 		{
-			delete (*it);
-			it = bullets.erase(it);
-		}
-		else it++;
-	}
+			char tecla = _getch();
 
-	for (int i = 0; i < asteroidsAmount; i++)
-	{
-		if (asteroids[i]->outOfBounds())
-		{
-			asteroids[i]->respawnAsteroid();					
-			playerStats.substractActiveAsteroids();
+			if (tecla == 'a' && ship->getPosition().getX() > 3) ship->travelLeft();
+			if (tecla == 'd' && ship->getPosition().getX() + ship->getSize().getWidth() < getScreenWidth() - 1) ship->travelRight();
+			if (tecla == 'e')
+			{
+				bullets.push_back(new Bullet(new Position(ship->getPosition().getX() + ship->getSize().getWidth() / 2 - 1, ship->getPosition().getY() - 1), new Size(1, 1)));
+			}
 		}
-		else
-			asteroids[i]->travelDown();
-	}
 
-	// Collision detection - Ship vs Asteroids
-	for (int i = 0; i < asteroidsAmount; i++)
-	{
-		if (ship->checkCollision(ship->getPosition(), ship->getSize(), asteroids[i]->getPosition(), asteroids[i]->getSize()))
-		{
-			((Ship*)ship)->takeDamage();
-			asteroids[i]->respawnAsteroid();			
-			playerStats.substractActiveAsteroids();
-		}
-	}
-
-	// Collision detection - Bullets vs Asteroids
-	for (int i = 0; i < asteroidsAmount; i++)
-	{
 		for (auto it = bullets.begin(); it != bullets.end();)
-		{			
-			if ((*it)->checkCollision((*it)->getPosition(), (*it)->getSize(), asteroids[i]->getPosition(), asteroids[i]->getSize()))
+		{
+			(*it)->travelUp();
+			if ((*it)->outOfBounds())
 			{
 				delete (*it);
 				it = bullets.erase(it);
-				asteroids[i]->respawnAsteroid();				
-				playerStats.substractActiveAsteroids();
-				((Ship*)ship)->addScore();
 			}
 			else it++;
 		}
-	}
 
-	if (((Ship*)ship)->getCurrentLives() == 0)
-	{
-		hud.showDefeat();
-		exit = true;		
-	}
-	else if (((Ship*)ship)->getCurrentScore() == 10)
-	{
-		hud.showVictory();
-		exit = true;
-	}
-	
+		for (int i = 0; i < asteroidsAmount; i++)
+		{
+			if (asteroids[i]->outOfBounds())
+			{
+				asteroids[i]->respawnAsteroid();
+			}
+			else
+				asteroids[i]->travelDown();
+		}
 
+		// Collision detection - Ship vs Asteroids
+		for (int i = 0; i < asteroidsAmount; i++)
+		{
+			if (ship->checkCollision(ship->getPosition(), ship->getSize(), asteroids[i]->getPosition(), asteroids[i]->getSize()))
+			{
+				((Ship*)ship)->takeDamage();
+				asteroids[i]->respawnAsteroid();
+			}
+		}
 
+		// Collision detection - Bullets vs Asteroids
+		for (int i = 0; i < asteroidsAmount; i++)
+		{
+			for (auto it = bullets.begin(); it != bullets.end();)
+			{
+				if ((*it)->checkCollision((*it)->getPosition(), (*it)->getSize(), asteroids[i]->getPosition(), asteroids[i]->getSize()))
+				{
+					delete (*it);
+					it = bullets.erase(it);
+					asteroids[i]->respawnAsteroid();
+					((Ship*)ship)->addScore();
+				}
+				else it++;
+			}
+		}
+
+		if (((Ship*)ship)->getCurrentLives() == 0)
+		{
+			hud.showDefeat();
+			exit = true;
+		}
+		else if (((Ship*)ship)->getCurrentScore() == 10)
+		{
+			hud.showVictory();
+			exit = true;
+		}
+	}
 }
 
--- Final_P1_Pierpaoli_Matias/Ejercicio 1/HUD.cpp
+++ Final_P1_Pierpaoli_Matias/Ejercicio 1/HUD.cpp
@@ -10,10 +10,16 @@ HUD::~HUD()
 
 void HUD::showVictory()
 {
+	goToCoordinates(getScreenWidth() / 2 - 6, getScreenHeight() / 2 + 6);
+	cout << "VICTORY!" << endl;
+	cin.get();
 }
 
 void HUD::showDefeat()
 {
+	goToCoordinates(getScreenWidth() / 2 - 6, getScreenHeight() / 2);
+	cout << "DEFEAT!" << endl;
+	cin.get();
 }
 
 void HUD::drawFrameHUD()
